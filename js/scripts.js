6ff23b1301a25e1c265bb4803f6cd94180aac714
HIMERA <PTANPNH722@gmail.com>
2024-08-28 10:37:43 +0300

    add JQuery code

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,20 @@
-
-// burger menu
-
-document.getElementById('menuToggle').addEventListener('click', function() {
-    const navbarNav = document.getElementById('navbarNav');
-    const navbarToggler = document.getElementById('menuToggle');
-    
-    // Toggle the class to show or hide the menu
-    navbarNav.classList.toggle('show');
-    
-    // Toggle the visibility of the hamburger button
-    navbarToggler.classList.toggle('hidden');
+// Burger menu toggle with jQuery
+$('#menuToggle').click(function() {
+    $('#navbarNav').toggleClass('show');
+    $('#menuToggle').toggleClass('hidden');
 });
 
-document.getElementById('menuClose').addEventListener('click', function() {
-    const navbarNav = document.getElementById('navbarNav');
-    const navbarToggler = document.getElementById('menuToggle');
-    
-    // Close the menu
-    navbarNav.classList.remove('show');
-    
-    // Show the hamburger button
-    navbarToggler.classList.remove('hidden');
+$('#menuClose').click(function() {
+    $('#navbarNav').removeClass('show');
+    $('#menuToggle').removeClass('hidden');
 });
 
-
-// language toggler 
-
-const buttons = document.querySelectorAll('.language-button');
-const items = document.querySelectorAll('.language-item');
-
-buttons.forEach((button, index) => {
-    const item = items[index]; // use index .language-item for .language-button
-
-    button.addEventListener('mouseover', () => {
-        item.classList.add('hovered');
-    });
-
-    button.addEventListener('mouseout', () => {
-        item.classList.remove('hovered');
-    });
-});
+// Language toggler with jQuery
+$('.language-button').hover(
+    function() {
+        $(this).parent().addClass('hovered');
+    },
+    function() {
+        $(this).parent().removeClass('hovered');
+    }
+);

3adf39fb7167184278228c7a932913318b1519d4
HIMERA <PTANPNH722@gmail.com>
2024-08-28 11:50:52 +0300

    black opacity bag fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-// Burger menu toggle with jQuery
+// Toggle the navigation menu and the menu button visibility
 $('#menuToggle').click(function() {
     $('#navbarNav').toggleClass('show');
     $('#menuToggle').toggleClass('hidden');
 });
 
+// Close the navigation menu and show the menu button
 $('#menuClose').click(function() {
     $('#navbarNav').removeClass('show');
     $('#menuToggle').removeClass('hidden');
 });
 
-// Language toggler with jQuery
+// Add or remove hover effect on language buttons
 $('.language-button').hover(
     function() {
         $(this).parent().addClass('hovered');
